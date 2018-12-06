91689bb9fe174c72187d4f67a792ed9e0bd7428e
alfha101 <ahamard@gmail.com>
2018-12-06 15:03:22 +0100

    security-assignment

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,7 +7,7 @@ class ServicesController < ApplicationController
   after_action :verify_authorized
   after_action :verify_policy_scoped, only: [:index]
 
-  require 'colorize'
+  # require 'colorize'
 
 
   def index
